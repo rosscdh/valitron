987fe081ae03b9c022bff89e2b2f16846088655b
Willem Wollebrants <willem@studiow.be>
2017-01-03 10:42:13 +0100

    use specific assert methods, test dot syntax

--- tests/Valitron/MapRulesTest.php
+++ tests/Valitron/MapRulesTest.php
@@ -14,32 +14,50 @@ class MapRulesTest extends BaseTestCase
 
         $v = new Validator(array());
         $v->mapFieldRules('myField', $rules);
-        $this->assertEquals(false, $v->validate());
+        $this->assertFalse($v->validate());
         $this->assertEquals(2, sizeof($v->errors('myField')));
 
         $v2 = new Validator(array('myField' => 'myVal'));
         $v2->mapFieldRules('myField', $rules);
-        $this->assertEquals(true, $v2->validate());
+        $this->assertTrue($v2->validate());
     }
 
-    public function testMapMultipleFieldsRules(){
+    public function testSingleFieldDot()
+    {
+
+        $v = new Valitron\Validator(array(
+            'settings' => array(
+                array('threshold' => 50),
+                array('threshold' => 90)
+            )
+        ));
+        $v->mapFieldRules('settings.*.threshold', array(
+            array('max', 50)
+        ));
+        
+        $this->assertFalse($v->validate());
+
+    }
+
+    public function testMapMultipleFieldsRules()
+    {
         $rules = array(
-            'myField1'=>array(
+            'myField1' => array(
                 'required',
                 array('lengthMin', 4)
             ),
-            'myField2'=>array(
+            'myField2' => array(
                 'required',
                 array('lengthMin', 5)
             )
         );
 
         $v = new Validator(array(
-            'myField1'=>'myVal'
+            'myField1' => 'myVal'
         ));
         $v->mapFieldsRules($rules);
 
-        $this->assertEquals(false, $v->validate());
+        $this->assertFalse($v->validate());
         $this->assertFalse($v->errors('myField1'));
         $this->assertEquals(2, sizeof($v->errors('myField2')));
 
